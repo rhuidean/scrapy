a96016d871961cc00b043d1cf2a73ce317b191e3
Jason Lee <rhuidean06@gmail.com>
2017-08-28 17:03:56 -0400

    add pipelines.py

--- full_architecture_spider/quotes_spider/pipelines.py
+++ full_architecture_spider/quotes_spider/pipelines.py
@@ -4,8 +4,12 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+# working with item
+# change setting.py file
 
 class QuotesSpiderPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+	def process_item(self, item, spider):
+		if item['h1_tag']:
+			item['h1_tag'] = item['h1_tag'][0].upper()
+		return item
