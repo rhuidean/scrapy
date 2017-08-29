e5cda0d58150c8642da5862012659ef1eebdfe8a
Jason Lee <rhuidean06@gmail.com>
2017-08-29 19:41:36 -0400

    add selenium project

---
+++ selen/selen/spiders/books.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BooksSpider(scrapy.Spider):
+    name = 'books'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        pass
